d95881017d29b20b691c3d8e25cb7d0de9285769
brend mark <brendo_mark@hotmail.com>
2025-02-19 17:55:38 -0300

    ajustes

--- receitas/views.py
+++ receitas/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     #return HttpResponse("Olá, mundo!")
-    return render(request,'receitas/home.html',context={'usuario':'brendo'})
+    return render(request,'receitas/paginas/home.html',context={'usuario':'brendo'})
 
